4494b10effbf805428860e9fdfd3946624ae5e4f
Andras Tim <andras.tim@gmail.com>
2015-01-20 10:43:28 +0100

    Revert "server: SQL independent workaround"
    This reverts commit 4d2d58b9afaf9d00ffecd5f6b0900047bdb1ab78.

--- server/app/models.py
+++ server/app/models.py
@@ -4,35 +4,6 @@ from wtforms.validators import Email
 from .server import db, bcrypt
 
 
-def yearly_reset_id(commit_function, retry_count=3):
-    def get_new_id(self, session):
-        return session.query(
-            db.func.ifnull(
-                db.func.max(self.__class__.id),
-                0
-            )
-        ).filter(
-            Work.year == self.year
-        ).first()[0] + 1
-
-    def wrapper(self, session):
-        if self.id:
-            commit_function(self, session)
-            return
-
-        error = None
-        for i in range(retry_count):
-            self.id = get_new_id(self, session)
-            try:
-                commit_function(self, session)
-                return
-            except sqlalchemy.exc.IntegrityError as error:
-                session.rollback()
-
-        raise Exception("Can not commit %d time(s)! %s" % (retry_count, str(error)))
-    return wrapper
-
-
 def set_yearly_reset_for_autoincrement_counter(table_object: sqlalchemy.sql.schema.Table,
                                                id_column_name: str="id",
                                                year_column_name: str="year"):
@@ -137,9 +108,12 @@ class Work(db.Model):
         self.name = name
 
     def __repr__(self):
-        return '<Work %r>' % self.full_id
+        return '<Work %r>' % self.id
 
-    @yearly_reset_id
-    def commit(self, session):
-        session.add(self)
-        session.commit()
+
+set_yearly_reset_for_autoincrement_counter(Work.__table__)
+
+
+
+# sys.path.extend(['/home/tia/codes/github-com/StoreKeeper/server']); from app.server import db; from app.models import *
+# w = Work(2014, "test"); db.session.add(w); db.session.commit()
