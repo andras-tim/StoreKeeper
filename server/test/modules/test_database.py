1f2128cf13b3022545054ccc42f0151e8634b723
Andras Tim <andras.tim@gmail.com>
2015-02-16 03:20:09 +0100

    server: Added common base class to tests of database maintenance

--- server/test/modules/test_database.py
+++ server/test/modules/test_database.py
@@ -7,24 +7,35 @@ from app.server import config
 from app.modules.database_maintenance import DatabaseMaintenance
 
 
-class TestDatabaseCreate(unittest.TestCase):
-    def tearDown(self):
-        _cleanup_db_and_migrate_repo()
+class CommonDatabaseMaintenanceTest(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls._cleanup_db_and_migrate_repo()
 
-    def test_create(self):
-        DatabaseMaintenance.create()
-
-        self.assertTrue(os.path.exists(config.App.MIGRATE_REPO_PATH))
-        self.assertEqual(0, DatabaseMaintenance.get_version())
-
-
-class TestDatabaseMigrate(unittest.TestCase):
     def setUp(self):
         DatabaseMaintenance.create()
 
     def tearDown(self):
-        _cleanup_db_and_migrate_repo()
+        CommonDatabaseMaintenanceTest._cleanup_db_and_migrate_repo()
 
+    @classmethod
+    def _cleanup_db_and_migrate_repo(cls):
+        db_path = re.sub(r'^[^:]+:///(.+)$', r'\1', config.Flask.SQLALCHEMY_DATABASE_URI)
+        if db_path != ":memory:" and os.path.exists(db_path):
+            os.remove(db_path)
+
+        if os.path.exists(config.App.MIGRATE_REPO_PATH):
+            shutil.rmtree(config.App.MIGRATE_REPO_PATH)
+
+
+class TestCreateWithDatabaseSupport(CommonDatabaseMaintenanceTest):
+    def test_create(self):
+        # The DatabaseMaintenance.create() call was ran in super().setUp()
+        self.assertTrue(os.path.exists(config.App.MIGRATE_REPO_PATH))
+        self.assertEqual(0, DatabaseMaintenance.get_version())
+
+
+class TestMigrateWithDatabaseSupport(CommonDatabaseMaintenanceTest):
     def test_migrate(self):
         DatabaseMaintenance.migrate()
         self.assertEqual(1, DatabaseMaintenance.get_version())
@@ -33,14 +44,11 @@ class TestDatabaseMigrate(unittest.TestCase):
         self.assertEqual(2, DatabaseMaintenance.get_version())
 
 
-class TestDatabaseSwitchVersion(unittest.TestCase):
+class TestSwitchVersionWithDatabaseSupport(CommonDatabaseMaintenanceTest):
     def setUp(self):
-        DatabaseMaintenance.create()  # ver=0
+        super().setUp()
         DatabaseMaintenance.migrate()  # ver=1
         DatabaseMaintenance.migrate()  # ver=2
-
-    def tearDown(self):
-        _cleanup_db_and_migrate_repo()
 
     def test_downgrade(self):
         DatabaseMaintenance.downgrade()
@@ -54,12 +62,3 @@ class TestDatabaseSwitchVersion(unittest.TestCase):
 
         DatabaseMaintenance.upgrade()
         self.assertEqual(2, DatabaseMaintenance.get_version())
-
-
-def _cleanup_db_and_migrate_repo():
-    db_path = re.sub(r'^[^:]+:///(.+)$', r'\1', config.Flask.SQLALCHEMY_DATABASE_URI)
-    if db_path != ":memory:" and os.path.exists(db_path):
-        os.remove(db_path)
-
-    if os.path.exists(config.App.MIGRATE_REPO_PATH):
-        shutil.rmtree(config.App.MIGRATE_REPO_PATH)
