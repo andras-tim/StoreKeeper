749d1ca8f9ffd12acf63d7203bf42ed374c2dcb4
Andras Tim <andras.tim@gmail.com>
2015-02-16 03:36:27 +0100

    server: Added test case for model of User

--- server/test/__init__.py
+++ server/test/__init__.py
@@ -2,3 +2,4 @@ import app
 app.test_mode = True
 
 from test.modules import *
+from test.test_models import *
---
+++ server/test/test_models.py
@@ -0,0 +1,20 @@
+import unittest
+
+from app.models import User
+from app.server import bcrypt
+
+
+class TestUser(unittest.TestCase):
+    _USER = "foo"
+    _PASSWORD = "secret_pass"
+    _EMAIL = "foo@bar"
+
+    def setUp(self):
+        self.user = User(TestUser._USER, TestUser._PASSWORD, TestUser._EMAIL)
+
+    def test_password_is_not_stored_plain_text(self):
+        self.assertNotEqual(TestUser._PASSWORD, self.user.password)
+        self.assertIsNotNone(self.user.password)
+
+    def test_password_is_able_to_check(self):
+        self.assertTrue(bcrypt.check_password_hash(self.user.password, TestUser._PASSWORD))
