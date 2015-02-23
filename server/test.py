e8b79b1ba9998a1dd6b588b4f15ad6e0b17b43f8
Andras Tim <andras.tim@gmail.com>
2015-02-23 12:07:16 +0100

    server: Fixed Travis build
    Removed unnecessary coveralls dev dependency

--- server/test.py
+++ server/test.py
@@ -2,26 +2,34 @@
 import os
 import unittest
 from coverage import coverage
+import sys
 
 import app
 
 
-if __name__ == '__main__':
-    cov = coverage()
+def main(args: list) -> bool:
+    run_by_ci = "--ci" in args
     suite = unittest.defaultTestLoader.discover(os.path.join(app.basedir, "test"))
 
+    os.chdir(app.basedir)
+    cov = coverage(config_file=os.path.join(app.basedir, ".coveragerc"))
     cov.start()
-    try:
-        unittest.TextTestRunner(verbosity=1).run(suite)
-    except:
-        pass
+    result = unittest.TextTestRunner(verbosity=1).run(suite)
     cov.stop()
-
-    print("\n\nCoverage Report:\n")
     cov.save()
-    cov.report()
 
-    print("\nHTML version: tmp/coverage/index.html")
-    cov.html_report()
+    if not run_by_ci:
+        print("\n\nCoverage Report:\n")
+        cov.report()
+        print("\nHTML version: tmp/coverage/index.html")
+        cov.html_report()
 
-    cov.erase()
+        cov.erase()
+
+    return len(result.errors) == 0
+
+
+if __name__ == '__main__':
+    if main(sys.argv[1:]):
+        sys.exit(0)
+    sys.exit(1)
