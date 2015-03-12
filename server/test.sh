801419159684d5ab4ec3b026c699af0eac818f64
Andras Tim <andras.tim@gmail.com>
2015-03-12 03:26:51 +0100

    server/test: Added fast mode with multi-threaded running
    Some test can not run in multi-threaded mode, but this save lot of money!

--- server/test.sh
+++ server/test.sh
@@ -11,5 +11,43 @@ function run_coverage()
     run_test --cov "${BASEDIR}/app" --cov-config "${BASEDIR}/.coveragerc" --cov-report=term-missing --cov-report=html "$@"
 }
 
-run_coverage --pdb
+function show_help()
+{
+    cat - << EOF
+Run tests and coverage
+
+$(basename "$0") [arg]
+
+Available arguments:
+  -h, --help            Show this help
+  -f, --fast            Run tests parallel (single threaded tests will be skipped)
+
+EOF
+}
+
+function get_count_of_cpu_cores()
+{
+    getconf _NPROCESSORS_ONLN
+}
+
+
+# Main
+FAST=false
+if [ $# -gt 0 ]
+then
+    case "$1" in
+        -h|--help)  show_help
+                    exit 0
+                    ;;
+        -f|--fast)  FAST=true
+                    ;;
+    esac
+fi
+
+if [ "${FAST}" == 'true' ]
+then
+    run_coverage -m 'not single_threaded' -n $[$(get_count_of_cpu_cores) + 1]
+else
+    run_coverage --pdb
+fi
 exit 0
