3cff8915427f114bde8055a8219682c86789f355
Andras Tim <andras.tim@gmail.com>
2015-04-23 05:37:51 +0200

    client: Updated language files

--- client/app/js/translations.js
+++ client/app/js/translations.js
@@ -1,5 +1,5 @@
 angular.module('gettext').run(['gettextCatalog', function (gettextCatalog) {
 /* jshint -W100 */
-    gettextCatalog.setStrings('hu', {"Auth check":"Bejelentkezés ellenőrzése","Enter search query":"Keresési kifejetés","LogOut":"Kijelentkezés","Login":"Bejelentkezés","Password":"Jelszó","Remember me":"Emlékezz rám","Search:":"Keresés:","Sign in":"Bejelentkezés","The user's password is required":"A jelszót kötelező megadni","The user's username is required":"Az email címet kötelező megadni","The username address is invalid":"Az email cím nem megfelelő","This field should not be left blank.":"Ez a mező nem maradhat üresen.","Username":"Felhasználónév","Welcome!":"Üdvözöllek!"});
+    gettextCatalog.setStrings('hu', {"Auth check":"Bejelentkezés ellenőrzése","Enter search query":"Keresési kifejetés","LogOut":"Kijelentkezés","Login":"Bejelentkezés","Password":"Jelszó","Remember me":"Emlékezz rám","Search:":"Keresés:","Sign in":"Bejelentkezés","The user's password is required":"A jelszót kötelező megadni","The user's username is required":"A felhasználónevet kötelező megadni","Username":"Felhasználónév","Welcome!":"Üdvözöllek!"});
 /* jshint +W100 */
 }]);
