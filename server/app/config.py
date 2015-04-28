e0081921bed34b30871bfd62837bece3fe74f6d3
Andras Tim <andras.tim@gmail.com>
2015-04-28 08:46:34 +0200

    server: Re-ordered functions in config

--- server/app/config.py
+++ server/app/config.py
@@ -5,6 +5,13 @@ from app import basedir, test_mode
 from app.modules.config import Config, ConfigObject
 
 
+def get_config() -> ConfigObject:
+    config_reader = __get_config_reader()
+    if test_mode:
+        return config_reader.read(used_config='TestingConfig')
+    return config_reader.read()
+
+
 def __get_config_reader() -> Config:
     config_path = os.path.join(basedir, 'config.yml')
     default_config_path = os.path.join(basedir, 'config_default.yml')
@@ -13,10 +20,3 @@ def __get_config_reader() -> Config:
         shutil.copy(default_config_path, config_path)
 
     return Config(config_path, config_variables={'BASEDIR': basedir})
-
-
-def get_config() -> ConfigObject:
-    config_reader = __get_config_reader()
-    if test_mode:
-        return config_reader.read(used_config='TestingConfig')
-    return config_reader.read()
