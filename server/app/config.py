8700aa03fd990be64f089dee6d92e8a01c39178f
Andras Tim <andras.tim@gmail.com>
2015-04-28 08:49:02 +0200

    server/config: Added config precheck for static sharing

--- server/app/config.py
+++ server/app/config.py
@@ -5,11 +5,26 @@ from app import basedir, test_mode
 from app.modules.config import Config, ConfigObject
 
 
+class ConfigurationError(Exception):
+    pass
+
+
 def get_config() -> ConfigObject:
+    config = __get_config()
+    __check_config(config)
+    return config
+
+
+def __get_config() -> ConfigObject:
     config_reader = __get_config_reader()
     if test_mode:
         return config_reader.read(used_config='TestingConfig')
     return config_reader.read()
+
+
+def __check_config(config: ConfigObject):
+    if config.App.ADMIN_PAGE and not config.App.SHARE_STATIC:
+        raise ConfigurationError('Have to enable App.SHARE_STATIC when App.ADMIN_PAGE is enabled')
 
 
 def __get_config_reader() -> Config:
