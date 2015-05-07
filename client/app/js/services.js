1a2f5d3b46a1e40b23e35512d8e98e374a3f1493
Andras Tim <andras.tim@gmail.com>
2015-05-07 06:56:32 +0200

    client: Added all currently known services

--- client/app/js/services.js
+++ client/app/js/services.js
@@ -3,9 +3,14 @@
 var appServices = angular.module('appServices', []);
 
 
-appServices.factory('SessionService', function (Restangular) {
-    return Restangular.service('session');
-});
+//appServices.factory('AcquisitionService', function (Restangular) {
+//    return Restangular.service('acquisitions');
+//});
+
+
+//appServices.factory('BarcodeService', function (Restangular) {
+//    return Restangular.service('barcodes');
+//});
 
 
 appServices.factory('ConfigService', function (Restangular) {
@@ -13,6 +18,36 @@ appServices.factory('ConfigService', function (Restangular) {
 });
 
 
+//appServices.factory('CustomerService', function (Restangular) {
+//    return Restangular.service('customers');
+//});
+
+
 appServices.factory('ItemService', function (Restangular) {
     return Restangular.service('items');
 });
+
+
+appServices.factory('SessionService', function (Restangular) {
+    return Restangular.service('session');
+});
+
+
+//appServices.factory('StocktakingService', function (Restangular) {
+//    return Restangular.service('stocktaking');
+//});
+
+
+//appServices.factory('UnitService', function (Restangular) {
+//    return Restangular.service('works');
+//});
+
+
+//appServices.factory('VendorService', function (Restangular) {
+//    return Restangular.service('vendors');
+//});
+
+
+//appServices.factory('WorkService', function (Restangular) {
+//    return Restangular.service('works');
+//});
