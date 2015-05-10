c5dcf4bfd26d49104e0f7bb6529e87aed81de2f4
Andras Tim <andras.tim@gmail.com>
2015-05-10 12:49:30 +0200

    client: Fixed filtering in nested fields
    The native Angular filter can filter in item.xxx only, however item.xxx.yyy
    does not yet.

--- client/app/js/filters.js
+++ client/app/js/filters.js
@@ -10,3 +10,36 @@ appFilters.filter('setTitle', ['PageFactory',
             return title;
         };
     }]);
+
+
+appFactories.filter('nestedIsContained', ['$parse', '$filter',
+    function($parse, $filter) {
+        function filter_in_all_fields(items, filters) {
+            var f = $filter('filter');
+            return f(items, {'$': filters['$']});
+        }
+
+        return function (items, filters) {
+            var itemsLeft;
+            if ('$' in filters) {
+                itemsLeft = filter_in_all_fields(items, filters)
+            } else {
+                itemsLeft = items.slice();
+            }
+
+            Object.keys(filters).forEach(function (model) {
+                if (model == '$') {
+                    return false;
+                }
+                var expected = filters[model].toLowerCase(),
+                    getter = $parse(model);
+
+                itemsLeft = itemsLeft.filter(function (item) {
+                    var actual = getter(item);
+                    return actual.toLowerCase().indexOf(expected) !== -1;
+                });
+            });
+
+            return itemsLeft;
+        };
+    }]);
