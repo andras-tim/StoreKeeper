fddfb2f00d0f210f3fd7e00ec9e8db70d619823c
Andras Tim <andras.tim@gmail.com>
2015-05-10 07:58:24 +0200

    server: Changed ALLOW_API_SIMPLE_AUTH default value to False
    This config have to false with current session management.

--- server/app/res/restfulApi.py
+++ server/app/res/restfulApi.py
@@ -5,7 +5,7 @@ class RestfulApiWithoutSimpleAuth(restful.Api):
     def unauthorized(self, response):
         """ Given a response, change it to ask for credentials """
 
-        allow_simple_auth = self.app.config.get("ALLOW_API_SIMPLE_AUTH", True)
+        allow_simple_auth = self.app.config.get("ALLOW_API_SIMPLE_AUTH", False)
         if allow_simple_auth:
             realm = self.app.config.get("HTTP_BASIC_AUTH_REALM", "flask-restful")
             challenge = "Basic realm=\"%s\"" % realm
