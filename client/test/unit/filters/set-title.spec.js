4806a0209b2dd6a9695f06a59aa2604723bb0a46
Andras Tim <andras.tim@gmail.com>
2015-08-04 03:22:16 +0200

    client/test: Added set-title.spec

---
+++ client/test/unit/filters/set-title.spec.js
@@ -0,0 +1,38 @@
+'use strict';
+
+describe('setTitleFilter', function () {
+    var test;
+
+    beforeEach(module('appFilters'));
+
+    beforeEach(function () {
+        test = this;
+
+        var mocks = {
+                'PageFactory': {
+                    'setPageTitle': function () {}
+                }
+            },
+
+            injectFilter = function () {
+                module(function ($provide) {
+                    $provide.value('PageFactory', mocks.PageFactory);
+                });
+                spyOn(mocks.PageFactory, 'setPageTitle').and.stub();
+
+                inject(function ($filter) {
+                    test.setTitle = $filter('setTitle');
+                });
+            };
+
+        this.mocks = mocks;
+        this.injectFilter = injectFilter;
+    });
+
+    it('setTitle() can set the title of page', function () {
+        test.injectFilter();
+
+        expect(test.setTitle('foo')).toEqual('foo');
+        expect(test.mocks.PageFactory.setPageTitle).toHaveBeenCalledWith('foo');
+    });
+});
