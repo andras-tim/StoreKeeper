b5764eda2fff482d7fbddc037ce5b8f48f76c52c
Andras Tim <andras.tim@gmail.com>
2015-08-06 05:27:37 +0200

    client/test: Added app-pager.spec

---
+++ client/test/unit/directives/app-pager.spec.js
@@ -0,0 +1,59 @@
+'use strict';
+
+describe('appPager', function () {
+    var test;
+
+    beforeEach(module('appDirectives', 'partials'));
+
+    beforeEach(function () {
+        test = this;
+
+        var mocks = {
+                'selectPage': function () {}
+            },
+
+            injectDirective = function () {
+                spyOn(mocks, 'selectPage').and.stub();
+
+                inject(function ($compile, $rootScope) {
+                    test.$compile = $compile;
+                    test.$scope = $rootScope.$new();
+
+                    test.$scope.selectPage = mocks.selectPage;
+
+                    test.container = helper.compileTemplate(test, '<app-pager />');
+                });
+            };
+
+        this.mocks = mocks;
+        this.injectDirective = injectDirective;
+    });
+
+    it('input box have to always contain the number of current page', function () {
+        test.injectDirective();
+
+        expect(test.container.find('input').val()).toEqual('');
+
+        test.$scope.currentPage = 1;
+        test.$scope.$apply();
+        expect(test.container.find('input').val()).toEqual('1');
+
+        test.$scope.currentPage = 42;
+        test.$scope.$apply();
+        expect(test.container.find('input').val()).toEqual('42');
+    });
+
+    it('input change have to synced to current page', function () {
+        test.injectDirective();
+
+        expect(test.mocks.selectPage).not.toHaveBeenCalled();
+
+        test.container.find('input').val('1');
+        test.container.find('input').triggerHandler('change');
+        expect(test.mocks.selectPage).toHaveBeenCalledWith('1');
+
+        test.container.find('input').val('42');
+        test.container.find('input').triggerHandler('change');
+        expect(test.mocks.selectPage).toHaveBeenCalledWith('42');
+    });
+});
