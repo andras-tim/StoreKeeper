025d1116d3c4414b22931e19ce4dd89417a2c432
Andras Tim <andras.tim@gmail.com>
2015-09-10 00:52:41 +0200

    client: Added table settings persisting directive

--- client/app/js/src/directives.js
+++ client/app/js/src/directives.js
@@ -259,6 +259,70 @@ appDirectives.directive('appIndentedFormGroup',
 
 /**
  * @ngdoc directive
+ * @name appTablePersist
+ * @restrict A
+ *
+ * @param {string} appTablePersist unique ID of persistent storage
+ *
+ * @description
+ * Persist table state to local storage and search of $location
+ */
+appDirectives.directive('appTablePersist',
+    function appTablePersist () {
+        return {
+            require: '^stTable',
+            restrict: 'A',
+            scope: {
+                'appTablePersist': '@'
+            },
+            link: function (scope, element, attr, ctrl) {
+                var configId = 'table_' + attr.appTablePersist;
+
+                function backupState(newValue, oldValue) {
+                    if (newValue === oldValue) {
+                        return;
+                    }
+
+                    var state = {
+                        'version': 1,
+                        'sort': newValue.sort
+                    };
+
+                    localStorage.setItem(configId, JSON.stringify(state));
+                }
+
+                function restoreState() {
+                    var currentState,
+                        stateJson,
+                        state;
+
+                    stateJson = localStorage.getItem(configId);
+                    if (!stateJson) {
+                        return;
+                    }
+
+                    state = JSON.parse(stateJson);
+                    currentState = ctrl.tableState();
+
+                    if (state.version === 1) {
+                        currentState.sort = state.sort;
+                    }
+
+                    ctrl.pipe();
+                }
+
+                scope.$watch(function () {
+                    return ctrl.tableState();
+                }, backupState, true);
+
+                restoreState();
+            }
+        };
+    });
+
+
+/**
+ * @ngdoc directive
  * @name appPageInput
  * @restrict E
  *
